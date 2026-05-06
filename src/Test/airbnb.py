494283d90c31fa73decc0fe3c90f899b013d3a76
leomf-dev <leonelmelendezfalcon@gmail.com>
2026-05-05 21:32:59 -0500

    data extraction

---
+++ src/Test/airbnb.py
@@ -0,0 +1,25 @@
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+from webdriver_manager.chrome import ChromeDriverManager
+
+opts = Options()
+opts.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+opts.add_argument("--headless")
+
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install()),
+    options=opts
+)
+
+driver.get('https://www.airbnb.com/')
+
+sleep(6)
+
+titulos = driver.find_elements(By.XPATH, '//div[@data-testid="listing-card-title"]')
+for titulo in titulos:
+    print(titulo.text)
